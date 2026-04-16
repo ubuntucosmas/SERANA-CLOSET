00b207d3847146435b5c6cf3d98e07a4a4cf2d93
Cosmas <cosmasasango12@gmail.com>
2026-04-16 17:03:29 +0300

    ui updates

--- resources/js/Components/MobileBottomNav.vue
+++ resources/js/Components/MobileBottomNav.vue
@@ -40,10 +40,12 @@ function isActive(routeName) {
                 <span class="text-[8px] font-bold uppercase tracking-widest">Circle</span>
             </Link>
 
-            <Link :href="route('profile.edit')" class="flex flex-col items-center gap-1 transition-all" :class="isActive('profile.*') ? 'text-primary' : 'text-white/40'">
-                <span class="material-symbols-outlined text-[24px]">person</span>
-                <span class="text-[8px] font-bold uppercase tracking-widest">Vault</span>
-            </Link>
+            <a href="https://www.tiktok.com/@serana904" target="_blank" class="flex flex-col items-center gap-1 transition-all text-white/40 hover:text-primary">
+                <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1.04-.1z"/>
+                </svg>
+                <span class="text-[8px] font-bold uppercase tracking-widest">TikTok</span>
+            </a>
 
         </div>
         
